44571a736dd9f1bf61298719a8f2830152d931fb
dangnhatquang <nhatquang251196@gmail.com>
2021-01-01 10:06:54 +0700

    Create a sample store management project by vue cli + add quasar and vue router + homepage, aboutus (future)

---
+++ vue-store-management/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="glossy">
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+          icon="menu"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-2"
+    >
+      <q-list>
+        <!-- SAMPLE HOME VUE ROUTER -->
+        <q-item clickable to="/">
+          <q-item-section avatar>
+            <q-icon name="home" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>HOME</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <!-- SAMPLE ABOUT US VUE ROUTER -->
+        <!-- <router-link to="/aboutUs" > -->
+        <q-item clickable to="/aboutUs">
+          <q-item-section avatar>
+            <q-icon name="face" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>ABOUT US</q-item-label>
+          </q-item-section>
+        </q-item>
+        <!-- </router-link> -->
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view></router-view>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'LayoutDefault',
+
+  data () {
+    return {
+      leftDrawerOpen: false,
+      menu:null
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ vue-store-management/src/router.js
@@ -0,0 +1,13 @@
+import AboutUs  from "./components/aboutUs"
+import Home from "./components/home"
+
+export default [
+    {
+        path:'/aboutUs',
+        component: AboutUs
+    },
+    {
+        path:'/',
+        component: Home
+    },
+]
